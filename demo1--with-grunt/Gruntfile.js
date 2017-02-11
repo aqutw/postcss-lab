59eaa754eaf63c2da0c8f0fe0e1d9d2f25eb53da
aqutw <aquarian.ex+github@gmail.com>
2017-02-11 11:50:49 +0800

    Done demo1--with-grunt

--- demo1--with-grunt/Gruntfile.js
+++ demo1--with-grunt/Gruntfile.js
@@ -1,9 +1,14 @@
+// https://webdesign.tutsplus.com/tutorials/postcss-quickstart-guide-grunt-setup--cms-24545
 module.exports = function(grunt) {
   grunt.initConfig({
  
     postcss: {
       options: {
         processors: [
+        require('autoprefixer')(),
+        // require('autoprefixer')()({browsers: ['last 1 version']}),
+        require('cssnext')(),
+        require('precss')()
         ]
       },
       dist: {
